4d9b08a4d78cb923736298845dc4fc6f442b8e7b
H0wler6 <H0wler6@proton.me>
2025-01-18 19:32:37 -0600

    Updated to put links in hamburger style menu and addded js for interaction.

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle the menu visibility
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
+
+    menuToggle.addEventListener("click", () => {
+        menu.classList.toggle("active");
+    });
+});
